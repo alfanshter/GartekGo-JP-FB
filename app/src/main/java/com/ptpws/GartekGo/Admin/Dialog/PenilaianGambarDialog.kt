ca5323f943f2f325872a111f4223abbdddc8abe0
anampict <anampict@gmail.com>
2025-06-19 00:00:06 +0700

    penilaiangambar dialog

--- app/src/main/java/com/ptpws/GartekGo/Admin/Dialog/PenilaianGambarDialog.kt
+++ app/src/main/java/com/ptpws/GartekGo/Admin/Dialog/PenilaianGambarDialog.kt
@@ -1,5 +1,6 @@
 package com.ptpws.GartekGo.Admin.Dialog
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,8 +22,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -33,25 +32,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.ptpws.GartekGo.Commond.poppinsfamily
+import com.ptpws.GartekGo.R
 
 @Composable
-fun NamaTopikDialog(
+fun PenilaianGambarDialog(
 ) {
-    var showDialognamatopik by remember { mutableStateOf(true) }
-    var topicText by remember { mutableStateOf("") }
+    var showDialogpenilaiangambar by remember { mutableStateOf(true) }
+    var tulisnilaiText by remember { mutableStateOf("") }
+    var keterangan by remember { mutableStateOf("") }
 
-    if (showDialognamatopik) {
+    if (showDialogpenilaiangambar) {
         Dialog(
-            onDismissRequest = { showDialognamatopik = false },
+            onDismissRequest = { showDialogpenilaiangambar = false },
             properties = DialogProperties(usePlatformDefaultWidth = false)
         ) {
             Box(
@@ -64,7 +68,7 @@ fun NamaTopikDialog(
                 Card(
                     modifier = Modifier
                         .fillMaxWidth(0.9f)
-                        .height(300.dp)
+                        .height(723.dp)
                         .padding(top = 20.dp),
                     shape = RoundedCornerShape(40.dp),
                     colors = CardDefaults.cardColors(containerColor = Color(0xFFEAF2FF))
@@ -73,23 +77,104 @@ fun NamaTopikDialog(
                         horizontalAlignment = Alignment.CenterHorizontally,
                         modifier = Modifier.padding(20.dp)
                     ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.gambartugas),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(300.dp)
+                                .clip(shape = RoundedCornerShape(10.dp))
+                        )
+                        Spacer(Modifier.height(23.dp))
+                        Button(
+                            onClick = { },
+                            modifier = Modifier
+                                .height(48.dp)
+                                .width(304.dp),
+                            shape = RoundedCornerShape(10.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = Color(0xff464646))
+                        ) {
+                            Text(
+                                "Unduh Gambar",
+                                fontFamily = poppinsfamily,
+                                fontWeight = FontWeight.Medium,
+                                fontSize = 16.sp,
+                                color = Color.White
+                            )
+                        }
+                        Spacer(Modifier.height(23.dp))
+
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Text(
+                                "Nilai",
+                                fontFamily = poppinsfamily,
+                                fontWeight = FontWeight.Medium,
+                                fontSize = 13.sp,
+                                color = Color.Black
+                            )
+                        }
+                        Spacer(Modifier.height(4.dp))
 
                         TextField(
-                            value = topicText,
-                            onValueChange = { topicText = it },
+                            value = tulisnilaiText,
+                            onValueChange = { tulisnilaiText = it },
                             placeholder = {
                                 (Text(
-                                    "Nama Topik",
+                                    "Tulis Nilai",
                                     fontFamily = poppinsfamily,
-                                    fontSize = 24.sp,
+                                    fontSize = 16.sp,
                                     fontWeight = FontWeight.SemiBold,
                                     color = Color.Gray
                                 ))
                             },
-                            shape = RoundedCornerShape(24.dp),
+                            shape = RoundedCornerShape(10.dp),
                             singleLine = true,
                             textStyle = TextStyle(
-                                fontSize = 18.sp,
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color(0xFF1B1B2F)
+                            ),
+                            colors = TextFieldDefaults.colors(
+                                focusedContainerColor = Color.White,
+                                unfocusedContainerColor = Color.White,
+                                disabledContainerColor = Color.White,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent,
+                                cursorColor = Color.Black
+                            ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(54.dp)
+                        )
+                        Spacer(Modifier.height(23.dp))
+
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Text(
+                                "Keterangan",
+                                fontFamily = poppinsfamily,
+                                fontWeight = FontWeight.Medium,
+                                fontSize = 13.sp,
+                                color = Color.Black
+                            )
+                        }
+                        Spacer(Modifier.height(4.dp))
+
+                        TextField(
+                            value = keterangan,
+                            onValueChange = { keterangan = it },
+                            placeholder = {
+                                (Text(
+                                    "Tulis Keterangan",
+                                    fontFamily = poppinsfamily,
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.SemiBold,
+                                    color = Color.Gray
+                                ))
+                            },
+                            shape = RoundedCornerShape(10.dp),
+                            singleLine = true,
+                            textStyle = TextStyle(
+                                fontSize = 16.sp,
                                 fontWeight = FontWeight.Bold,
                                 color = Color.Black
                             ),
@@ -104,41 +189,36 @@ fun NamaTopikDialog(
                             ),
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .height(106.dp)
+                                .height(54.dp)
                         )
 
                         Spacer(modifier = Modifier.height(16.dp))
 
                         Row(
                             modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceEvenly
+                            horizontalArrangement = Arrangement.Start
                         ) {
-                            Button(
-                                onClick = {
-                                    topicText = ""
-                                    showDialognamatopik = false
-                                },
-                                colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
-                                shape = RoundedCornerShape(12.dp),
-                                modifier = Modifier.width(120.dp)
-                            ) {
-                                Text("HAPUS", fontFamily = poppinsfamily, fontSize = 18.sp, fontWeight = FontWeight.SemiBold, color = Color.White)
-                            }
 
                             Button(
                                 onClick = {
                                     // Simpan data di sini jika perlu
-                                    showDialognamatopik = false
+
                                 },
                                 colors = ButtonDefaults.buttonColors(
                                     containerColor = Color(
                                         0xFF00C853
                                     )
                                 ),
-                                shape = RoundedCornerShape(12.dp),
-                                modifier = Modifier.width(120.dp)
+                                shape = RoundedCornerShape(20.dp),
+                                modifier = Modifier.width(120.dp).height(60.dp)
                             ) {
-                                Text("SIMPAN",fontFamily = poppinsfamily, fontSize = 18.sp, fontWeight = FontWeight.SemiBold, color = Color.White)
+                                Text(
+                                    "SIMPAN",
+                                    fontFamily = poppinsfamily,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.SemiBold,
+                                    color = Color.White
+                                )
                             }
                         }
                     }
@@ -156,7 +236,7 @@ fun NamaTopikDialog(
                         colors = CardDefaults.cardColors(containerColor = Color.White),
                         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
                     ) {
-                        IconButton(onClick = { showDialognamatopik = false }) {
+                        IconButton(onClick = { showDialogpenilaiangambar = false }) {
                             Icon(
                                 imageVector = Icons.Default.Close,
                                 contentDescription = "Tutup",
@@ -175,8 +255,6 @@ fun NamaTopikDialog(
 @Preview(showBackground = true)
 @Composable
 private fun NamaTopikDialogPreview() {
-    NamaTopikDialog(
-
-    )
+    PenilaianGambarDialog()
 
 }
