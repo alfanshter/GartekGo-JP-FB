d4be2498544e756b45829d63fb36d989d7047ce1
anampict <anampict@gmail.com>
2025-07-19 11:26:40 +0700

    revisi upload project

--- app/src/main/java/com/ptpws/GartekGo/Admin/model/UploadModel.kt
+++ app/src/main/java/com/ptpws/GartekGo/Admin/model/UploadModel.kt
@@ -4,9 +4,14 @@ import com.google.firebase.Timestamp
 
 data class UploadModel(
     val uid: String = "",
+    val nama: String = "",
+    val kelas: String = "",
+    val program_keahlian: String = "",
     val id_topik: String = "",
+    val nama_topik: String = "",
+    val nomor_topik: Int = 0,
     val imageUrl: String = "",
     val timestamp: Timestamp = Timestamp.now(),
-    val status: String? = null,
+    val status: Boolean = false,
     val nilai: Int? = null
 )
